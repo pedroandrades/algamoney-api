3b6da2bcf278c1a5bda4f41c137939d08971558e
pedroandrades <pehfass@gmail.com>
2021-01-27 10:19:29 -0300

    Posting post added

--- src/main/java/com/example/algamoney/api/controller/PostingController.java
+++ src/main/java/com/example/algamoney/api/controller/PostingController.java
@@ -2,13 +2,13 @@ package com.example.algamoney.api.controller;
 
 import com.example.algamoney.api.model.Posting;
 import com.example.algamoney.api.repository.PostingRepository;
+import com.example.algamoney.api.util.HeaderUtil;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
+import javax.validation.Valid;
 import java.util.List;
 import java.util.Optional;
 
@@ -32,5 +32,12 @@ public class PostingController {
     public ResponseEntity<?> findById(@PathVariable Long id){
         Optional<Posting> posting = postingRepository.findById(id);
         return posting.isPresent() ? ResponseEntity.ok(posting) : ResponseEntity.notFound().build();
+    }
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public ResponseEntity<Posting> create(@Valid @RequestBody Posting posting) {
+        Posting savedPosting = postingRepository.save(posting);
+        return ResponseEntity.created(HeaderUtil.addLocation(savedPosting.getId())).body(savedPosting);
     }
 }
